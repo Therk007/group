0d105aa4587be694ea82bc108c907802f60c3ab1
Therk007 <tgvipkamisama@gmail.com>
2025-03-29 13:45:29 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-BOT_TOKEN = "7879783137:AAGvmgnNeKasW-kAcdfkWy23Ij2pgTC8tMw"  # Get from @BotFather
-ADMIN_ID = ["1549748318"]      # Your Telegram user ID
-CHANNEL_USERNAME = "@RAJKHILCHI"  # Channel users must join
-FEEDBACK_CHANNEL = "@RAAZKHILCHI"  # Where screenshots go
-ALLOWED_GROUPS = ["-1002654300042"]  # Default group ID
